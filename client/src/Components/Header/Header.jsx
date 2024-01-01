00b2ae9f8cf76a8f41fbe1f8f7f7d757352c9708
Tushar <tusharofcl@gmail.com>
2024-01-01 23:42:53 +0530

    feat:logo_link_error

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -53,7 +53,7 @@ const Header = () => {
         </div>
       </div>
       <div className={styles.mobHeader}>
-        <NavLink to="/home">
+        <NavLink to="/">
           <img src="/images/moblogo.png" alt="Brand Logo" />
         </NavLink>
       </div>
